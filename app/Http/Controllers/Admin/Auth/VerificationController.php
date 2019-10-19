9b6ff411fac0b98671dcbe0af29d4d812d83846d
Petro <kokpetro15@gmail.com>
2019-10-20 00:18:03 +0300

    redirect after login

--- app/Http/Controllers/Admin/Auth/VerificationController.php
+++ app/Http/Controllers/Admin/Auth/VerificationController.php
@@ -25,7 +25,8 @@ class VerificationController extends Controller
      *
      * @var string
      */
-    protected $redirectTo = '/';
+//    protected $redirectTo = '/';
+    protected $redirectTo = '/admin';
 
     /**
      * Create a new controller instance.
